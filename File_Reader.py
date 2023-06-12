78f59dfdb90ce5e30b51585482300e00ba259834
Haris Mahmood <haris.mahmood@uconn.edu>
2023-06-12 11:17:09 -0400

    Update file_reader.py

--- File_Reader.py
+++ File_Reader.py
@@ -6,11 +6,11 @@ def test_file_generator(data):
 
     if isinstance(data, list):
         for index, item in enumerate(data):
-            file_name = f"test_case_{index}.txt"
+            file_name = f"test_case_{index}.json"
             with open(file_name, 'w') as f:
                 f.write(json.dumps(item)) 
     else:
-        print("🚨🚨🚨🚨🚨🚨🚨🚨no array🚨🚨🚨🚨🚨🚨🚨🚨")
+        print("🚨❌🚨❌🚨❌no array❌🚨❌🚨❌🚨")
 
 
 input_data = 'testing_data.json'
