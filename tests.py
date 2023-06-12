7b1f9fb09d557524de4424faa3f50c1c083c0f32
Haris Mahmood <haris.mahmood@uconn.edu>
2023-06-12 08:44:43 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,19 +5,18 @@ from create_files_from_json import create_files_from_json_folder
 
 
 class TestJsonFileHandling(unittest.TestCase):
-    def setUp(self):
+    def create_temp_environment(self):
         self.test_folder = 'test_data'
         os.makedirs(self.test_folder, exist_ok=True)
 
-    def tearDown(self):
+    def delete_temp_environment(self):
         os.system(f"rm -rf {self.test_folder}")
 
     def create_json_file(self, file_path, data):
         with open(file_path, 'w') as f:
             json.dump(data, f)
 
-    def test_create_files_from_json_folder(self):
-        # Create sample JSON files
+    def testing_create_json_file(self):
         json_data1 = [
             {
                 "name": "John",
@@ -40,15 +39,12 @@ class TestJsonFileHandling(unittest.TestCase):
         self.create_json_file(f"{self.test_folder}/file1.json", json_data1)
         self.create_json_file(f"{self.test_folder}/subfolder1/file2.json", json_data2)
 
-        # Call the function to test
         create_files_from_json_folder(self.test_folder)
 
-        # Check if the files are created as expected
         self.assertTrue(os.path.exists(f"{self.test_folder}/file1.json_0.txt"))
         self.assertTrue(os.path.exists(f"{self.test_folder}/file1.json_1.txt"))
         self.assertTrue(os.path.exists(f"{self.test_folder}/subfolder1/file2.json_0.txt"))
 
-        # Check the contents of the created files
         with open(f"{self.test_folder}/file1.json_0.txt", 'r') as f:
             file1_0_data = json.load(f)
             self.assertEqual(file1_0_data, json_data1[0])
